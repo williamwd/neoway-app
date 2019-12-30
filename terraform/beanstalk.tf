e215b01c1a1ee04da6779faa1b124c84500c7318
William Monteiro <william@timonteiro.com>
2019-12-30 02:18:40 -0300

    Infrastructure provisioning

---
+++ terraform/beanstalk.tf
@@ -0,0 +1,53 @@
+resource "aws_elastic_beanstalk_application" "ebs-app" {
+  name        = "${var.key}-beanstalk"
+  description = "Application test"
+}
+
+data "aws_elastic_beanstalk_solution_stack" "single_docker" {
+  most_recent = true
+  name_regex = "^64bit Amazon Linux (.*) v(.*) running Docker (.*)$"
+}
+
+
+resource "aws_elastic_beanstalk_environment" "ebs-env" {
+    name = "${var.key}-env"
+    application = aws_elastic_beanstalk_application.ebs-app.name
+    tier = "WebServer"
+    solution_stack_name = data.aws_elastic_beanstalk_solution_stack.single_docker.name
+
+    setting {
+        namespace = "aws:elasticbeanstalk:environment"
+        name = "ServiceRole"
+        value = aws_iam_role.neoway-app-role.arn
+    }
+
+    setting {
+        namespace = "aws:elasticbeanstalk:environment"
+        name = "EnvironmentType"
+        value = "SingleInstance"
+    }
+
+    setting {
+        namespace = "aws:autoscaling:launchconfiguration"
+        name = "IamInstanceProfile"
+        value = aws_iam_instance_profile.ec2-ecr-profile.name
+    }
+
+    setting {
+        namespace = "aws:ec2:vpc"
+        name = "VPCId"
+        value = aws_vpc.neoway-app-vpc.id
+    }
+
+    setting {
+        namespace = "aws:ec2:vpc"
+        name = "Subnets"
+        value = aws_subnet.neoway-app-subnet.1.id
+    }
+
+    setting {
+        namespace = "aws:autoscaling:launchconfiguration"
+        name = "SecurityGroups"
+        value = aws_security_group.neoway-app-ebs-sg.id
+    }
+}
---
+++ terraform/pipeline.tf
@@ -0,0 +1,101 @@
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = "${var.key}-pipeline-bucket"
+  acl = "private"
+}
+
+resource "aws_codebuild_project" "neoway-app-codebuild" {
+  name          = "neoway-app-codebuild"
+  description   = "Neoway app codebuild project"
+  build_timeout = "5"
+  service_role  = aws_iam_role.neoway-app-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type     = "S3"
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:3.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = "true"
+  }
+
+  source {
+    type = "CODEPIPELINE"
+  }
+
+  tags = {
+    name = "neoway-app-build"
+  }
+}
+
+resource "aws_codepipeline" "neoway-app-pipeline" {
+    name     = "nw-pipeline"
+    role_arn = aws_iam_role.neoway-app-role.arn
+
+    artifact_store {
+        location = aws_s3_bucket.codepipeline_bucket.bucket
+        type     = "S3"
+    }
+
+    stage {
+        name = "Source"
+
+        action {
+            name             = "Source"
+            category         = "Source"
+            owner            = "ThirdParty"
+            provider         = "GitHub"
+            version          = "1"
+            output_artifacts = ["source_output"]
+
+            configuration = {
+                Owner  = var.repo_owner
+                Repo   = var.repo_name
+                Branch = var.repo_branch
+                OAuthToken = var.github_auth
+            }
+        }
+    }
+
+    stage {
+    name = "Build"
+
+        action {
+            name             = "Build"
+            category         = "Build"
+            owner            = "AWS"
+            provider         = "CodeBuild"
+            input_artifacts  = ["source_output"]
+            output_artifacts = ["build_output"]
+            version          = "1"
+
+            configuration = {
+                ProjectName = aws_codebuild_project.neoway-app-codebuild.name
+            }
+        }
+    }
+
+    stage {
+        name = "Deploy"
+
+        action {
+            name            = "Deploy"
+            category        = "Deploy"
+            owner           = "AWS"
+            provider        = "ElasticBeanstalk"
+            version         = "1"
+            input_artifacts = ["build_output"]
+
+            configuration = {
+                ApplicationName = aws_elastic_beanstalk_application.ebs-app.name
+                EnvironmentName = aws_elastic_beanstalk_environment.ebs-env.name
+            }
+        }
+    }
+}
